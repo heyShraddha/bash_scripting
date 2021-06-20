feaa1bc5b9d360ad1b55db00bf717d6019dbf6ed
heyShraddha <68199625+heyShraddha@users.noreply.github.com>
2021-06-20 18:45:21 +0530

    Updated sample.sh

--- sameln.sh
+++ sameln.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #Part1
 declare -A hash_map
 
@@ -23,48 +23,70 @@ ARG_COUNT=${#ARGUMENTS[@]}
 
 if [ $ARG_COUNT != 1 ]
 then
-    echo "Correct usage is: sameln.sh DIRECTORY_PATH"
+    echo "Command accepts only one argument, which should be directory"
+    echo "Correct usage is: sameln.sh DIRECTORY_PATH" #It takes only one argument
     exit
 fi
 
 # echo "# Arg Count is $ARG_COUNT"
 
+
+clear
+echo "          ______          _     _____             "
+echo "          | ___ \        | |   |_   _|            "
+echo "          | |_/ /_ _ _ __| |_    | |_      _____  "
+echo "          |  __/ _  |  __| __|   | \ \ /\ / / _ \ "
+echo "          | | | (_| | |  | |_    | |\ V  V / (_) |"
+echo "          \_|  \__,_|_|   \__|   \_/ \_/\_/ \___/ "
+
 DIR_NAME=${ARGUMENTS[0]}
-
-echo "You are trying to access directory: $DIR_NAME"
-if ! [ -d $DIR_NAME ]
+echo "---------------------------------------------------------"
+echo "Performing operations under directory: $DIR_NAME" #displays the user parameter passed, for better user experience
+if ! [ -d $DIR_NAME ] #check for the parameter passed
 then
-	echo "Directory doesn't exist"
-	exit 2
-fi	
-cd $DIR_NAME
-for filee in $(ls -a);do
+        echo "Directory doesn't exist" 
+        exit 2
+fi
+cd $DIR_NAME  #moving inside the directory passsed
+echo "---------------------------------------------------------"
+for filee in *;do #checks for everything inside the directory passed in argument
+	  
+        if [[ -f $filee  ]]; # checks, if it is file or directory --> -f is for file, -d is for directory
+	then
+                if   [[ -r $filee ]]; # checks, if the file has read permissions --> -r is for read 
+                then
+                        if  [[ -w $filee ]]; # checks, if the file has write permissions --> -w is for write 
+                        then
 
-	if [ -f $filee ] 
-	then 
-		if   [ -r $filee ] 
-		then
-	   		if  [ -w $filee ]
-			then
+                                if ! [[ -L $filee ]]; #ignoring the file having symbolic links
+                                then
+                                        echo "---------------------------------------------------------"
+                                        echo "Handling File with the file name:  $filee"
+                                        hashh=$(md5sum ./"$filee" | awk '{ print $1 }') #taking out only the first argument, which is hash value
+                                        #echo "$hashh -> $filee"
+                                        if [ "${hash_map[$hashh]}"  = "" ] # checks, if the hash already exist in hash map
+                                        then
+                                                echo "Added $filee to the Hash Map" # prints the file whose hash doesnt exist in map
+                                                hash_map[$hashh]="$filee" #defines the key value pair { hash value and filename }
+                                        else
 
-				if ! [ -L $filee ]
-				then
-
-					hashh=$(md5sum "./$filee" | awk '{ print $1 }')
-					echo "$hashh -> './$filee' "
-					if [ "${hash_map[$hashh]}"  = "" ]
-					then
-						echo "added '$filee'"
-						hash_map[$hashh]='./$filee'
-					else
-						rm './$filee'
-						ln "./${hash_map[$hashh]}" "./$filee"
-						echo "hard link $filee to ${hash_map[$hashh]}"
-					fi	
-				fi
-			fi
-		fi
-
-	fi		
-		
-done	
+                                                rm ./"$filee" #removing the file which is the duplicate 
+                                                ln ./"${hash_map[$hashh]}" ./"$filee" #added a hard link to the file with the same name of deleted file
+                                                echo "Hard link $filee to ${hash_map[$hashh]}" #prints
+                                        fi
+                                        #else
+                                                #Silently Ignoring as it is symbolically linked" 
+                                fi
+                                
+                        else
+                                echo "The file $filee does not have write permission. Please provide, if you want to run this script for the same"
+                        fi      
+                else
+                        echo "The file $filee does not have read permission. Please provide, if you want to run this script for the same"     
+                fi  
+        #else
+                #Silently Ignoring as it is a directory" 
+        fi
+done
+echo "---------------------------------------------------------"
+echo "---------------------------------------------------------"                         
